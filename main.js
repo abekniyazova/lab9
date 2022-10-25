0a481ee80ccde956c8f762abbe88d017e43bea59
Azhar <azhar.bekniyazova@nu.edu.kz>
2022-10-25 23:38:18 +0600

    Lab work

---
+++ main.js
@@ -0,0 +1,22 @@
+function perimeter(a, b){
+    let p=2*(a+b)
+    console.log(p)
+}
+
+perimeter(5, 7)
+
+function sumSquares(a, b){
+    let s=(a*a) + (b*b)
+    console.log(s)
+}
+
+sumSquares(5, 7)
+
+function checkNumber(a){
+    if (a > 0) {
+    console.log(a + " is positive")
+    }
+    
+}
+
+checkNumber(7)
